f24a09d7af23491f2c3890a3dba18c2c62f785af
duvian <duvianmartin@gmail.com>
2024-03-22 14:45:53 -0500

    carpeta restaurada

---
+++ Controllers/PrestamoController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+    namespace mvc.controllers{
+    
+        public class PrestamoController : Controller{
+            public IActionResult Index(){
+                return View();
+            }
+                        public IActionResult Create(){
+                return View();
+            }
+
+            public IActionResult Delete(){
+                return View();
+            }
+        }
+    }
